963154f75d2da3b0afcb0a3b7b679e69b82c8300
Connor VanTress <11cvantress@gmail.com>
2024-03-06 09:10:45 -0600

    FizzBuzz added for numbers divisible by 5 and 3. Comments added

--- FizzBuzzpy.py
+++ FizzBuzzpy.py
@@ -4,10 +4,15 @@
 ## Loop through each number 0 to 100
 for num in range(0, 100):
 
+    #check if number is divisible by 3 and 5
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+
     #Check if num is divisible by 3
-    #Check if num is divisible by 5
-    if num % 3 == 0:
+    elif num % 3 == 0:
         print("Fizz")
+
+    #Check if num is divisible by 5
     elif num % 5 == 0:
         print("Buzz")
     else:

fa06a86e0ec385809c8f805f984f64968272351b
Connor VanTress <11cvantress@gmail.com>
2024-03-06 08:52:17 -0600

    Fizz for numbers divisible by 3 added

--- FizzBuzzpy.py
+++ FizzBuzzpy.py
@@ -4,6 +4,6 @@
 ## Loop through each number 0 to 100
 for num in range(0, 100):
 
-    ## Print each number
-    
-    print(num)
+    #Check if num is divisble by 3
+    if num % 3 == 0:
+        print("Fizz")

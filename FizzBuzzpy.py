0a7ed781f2616282fbb08ebded5c3ec2fed8f563
Connor VanTress <11cvantress@gmail.com>
2024-03-06 09:01:43 -0600

    Buzz for numbers divisble by 5

--- FizzBuzzpy.py
+++ FizzBuzzpy.py
@@ -4,8 +4,11 @@
 ## Loop through each number 0 to 100
 for num in range(0, 100):
 
-    #Check if num is divisble by 3
+    #Check if num is divisible by 3
+    #Check if num is divisible by 5
     if num % 3 == 0:
         print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
     else:
         print(num)
